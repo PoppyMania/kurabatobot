0062eff0de13d1d814a02c59a141b2d4da3e8837
poppymania <okgoglehisiri@gmail.com>
2020-08-02 23:20:37 +0900

    tag分けテスト

--- discordbot.py
+++ discordbot.py
@@ -6,10 +6,12 @@ import traceback
 from datetime import datetime
 from discord.ext import tasks
 
-bot = commands.Bot(command_prefix='/')
+prefix = '/'
+
+bot = commands.Bot(command_prefix=prefix)
 token = os.environ['DISCORD_BOT_TOKEN']
 
-     
+
 @bot.command()
 async def jantama(ctx):
     """じゃんたま公式HP"""
@@ -90,11 +92,11 @@ async def yabaiwayo(ctx):
     """ヤバいわよ.gif"""
     await ctx.send('https://img.animanch.com/2020/05/1588593715655.gif') 
     await ctx.message.delete()
-     
+    
 @bot.command()
 async def uranai(ctx):
     """占い"""
-     #レスポンスされる運勢のリストを作成
+    #レスポンスされる運勢のリストを作成
     unsei = ["大吉", "中吉", "吉", "末吉", "小吉", "凶", "大凶"]
     choice = random.choice(unsei) #randomモジュールでunseiリストからランダムに一つを選出
     await ctx.send(choice)
@@ -102,142 +104,147 @@ async def uranai(ctx):
 @bot.command()
 async def janken(ctx):
     """じゃんけん"""
-     #レスポンスされる運勢のリストを作成
+    #レスポンスされる運勢のリストを作成
     janken = ["グー", "チョキ", "パー"]
     choice = random.choice(janken) #randomモジュールでunseiリストからランダムに一つを選出
     await ctx.send(choice)
-     
-    # プリコネキャラ
-@bot.command()
-async def kurisu(ctx):
-    """クリス"""
-    await ctx.send('https://gyazo.com/39f3bddc360ed09f20431c79d809e3fb')
-    await ctx.message.delete()
-@bot.command()
-async def mizumakoto(ctx):
-    """マコト(サマー)"""
-    await ctx.send('https://i.gyazo.com/6e1c942142ce952e7b15c8f1aa6e6d73 \n 水が弾けたら')
-    await ctx.message.delete()
-@bot.command()
-async def makoto(ctx):
-    """マコト"""
-    await ctx.send('https://i.gyazo.com/016e0804ce330f11c6fcf75a60f89277 \n 黄色の鳴き声が消えたら')
-    await ctx.message.delete()
-@bot.command()
-async def muimi(ctx):
-    """ムイミ"""
-    await ctx.send('https://i.gyazo.com/016e0804ce330f11c6fcf75a60f89277 \n 左斜め後ろに飛んだ時')
-    await ctx.message.delete()
-@bot.command()
-async def tamaki(ctx):
-    """タマキ"""
-    await ctx.send('https://i.gyazo.com/ede0b883427fbbe2dbd944d03f0d3030')
-    await ctx.message.delete()     
-@bot.command()
-async def onon(ctx):
-    """ニノン(オーエド)"""
-    await ctx.send('https://gyazo.com/9ef7490093e2ce4114fc33b49b68a9f9')
-    await ctx.message.delete()
-@bot.command()
-async def mizuna(ctx):
-    """スズナ(サマー)"""
-    await ctx.send('https://i.gyazo.com/2b2c3b9dc2856a1d55d167bf920af87d')
-    await ctx.message.delete()
-@bot.command()
-async def neneka(ctx):
-    """ネネカ"""
-    await ctx.send('https://i.gyazo.com/588cb732059044e1be0712694eadcffd')
-    await ctx.message.delete()
-@bot.command()
-async def puripeko(ctx):
-    """ペコリーヌ(プリンセス)"""
-    await ctx.send('https://gyazo.com/0f0514e31d8b6136f87c33184b649b8d \n 切った後に剣を振り上げたタイミングでバフ付与')
-    await ctx.message.delete()
-@bot.command()
-async def mizukyaru(ctx):
-    """キャル(サマー)"""
-    await ctx.send('https://i.gyazo.com/cf8625a0f3fbe7ce5a47f00e2305568a')
-    await ctx.message.delete()
-@bot.command()
-async def purikoro(ctx):
-    """コッコロ(プリンセス)"""
-    await ctx.send('https://gyazo.com/04ffe3e6e434d79c816a837e435c1ff1')
-    await ctx.message.delete()    
-@bot.command()
-async def harokyo(ctx):
-    """キョウカ(ハロウィン)"""
-    await ctx.send('https://i.gyazo.com/60d59c64eb5c8a9443bd4de074cc03de \n 箒を突き刺した時/箒を振り終わった後')
-    await ctx.message.delete()
-@bot.command()
-async def mizusaren(ctx):
-    """サレン(サマー)"""
-    await ctx.send('https://gyazo.com/c22d7996cb2d8e91ae7308b53c66d6d4 \n 右を向いた時')
-    await ctx.message.delete()
-@bot.command()
-async def nyukkoro(ctx):
-    """コッコロ(ニューイヤー)"""
-    await ctx.send('https://gyazo.com/c29e006bf82ce2fb0e3b44c06405a9ce \n 左を向いた時')
-    await ctx.message.delete()    
-@bot.command()
-async def haromimi(ctx):
-    """ミミ(ハロウィン)"""
-    await ctx.send('https://i.gyazo.com/42f21611ee06890d48f4ffd4d38e6476 \n ぴょん ぴょん ぴょこ←ｺｺ')
-    await ctx.message.delete()
-@bot.command()
-async def bazuru(ctx):
-    """シズル(バレンタイン)"""
-    await ctx.send('https://i.gyazo.com/87b47efe83e874247b66519ce2caa78b')
-    await ctx.message.delete()
-@bot.command()
-async def kuritika(ctx):
-    """チカ(クリスマス)"""
-    await ctx.send('https://i.gyazo.com/d9b5fcdfca2cac56f82cfd1c9feefd9d \n 足元に魔法陣が出たら/tp配布の数字を見る')
-    await ctx.message.delete()    
-@bot.command()
-async def yuni(ctx):
-    """ユニ"""
-    await ctx.send('https://gyazo.com/67a7c23623583779f069e697df22aa23 \n どちらもジャンプ着地時')
-    await ctx.message.delete()
-@bot.command()
-async def rei(ctx):
-    """レイ"""
-    await ctx.send('https://i.gyazo.com/4dd4fbfc1687f4529e2f474a84c7a991')
-    await ctx.message.delete()
-@bot.command()
-async def suzuna(ctx):
-    """スズナ"""
-    await ctx.send('https://gyazo.com/0f21de3dba9d6d69dee31ef7991b71fc')
-    await ctx.message.delete()
-@bot.command()
-async def an(ctx):
-    """アン"""
-    await ctx.send('https://gyazo.com/4771ba5961617b5d66e8fe2aacdc3562')
-    await ctx.message.delete()
-@bot.command()
-async def misato(ctx):
-    """ミサト"""
-    await ctx.send('https://gyazo.com/d759d75e36acd6942f1e20d5bf5654ba')
-    await ctx.message.delete() 
-@bot.command()
-async def mizumaho(ctx):
-    """マホ(サマー)"""
-    await ctx.send('https://gyazo.com/2848612666b463901e2960c0282685b5')
-    await ctx.message.delete()
-@bot.command()
-async def haromiso(ctx):
-    """ミソギ(ハロウィン)"""
-    await ctx.send('https://gyazo.com/667afa5b86200885f706cd8cf65e7651')
-    await ctx.message.delete()
-@bot.command()
-async def hiyori(ctx):
-    """ヒヨリ"""
-    await ctx.send('https://gyazo.com/0fceb161f3ddeb1807f048271416525f')
-    await ctx.message.delete()
-@bot.command()
-async def mizuhatsune(ctx):
-    """ハツネ(サマー)"""
-    await ctx.send('https://gyazo.com/84891d66caf2de0092a9041020d9145e')
-    await ctx.message.delete()  
+        
+        # プリコネキャラ
+class PrGif(commands.Cog, name = 'スキルモーション'):
+
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+    @bot.command()
+    async def kurisu(ctx):
+        """クリス"""
+        await ctx.send('https://gyazo.com/39f3bddc360ed09f20431c79d809e3fb')
+        await ctx.message.delete()
+    @bot.command()
+    async def mizumakoto(ctx):
+        """マコト(サマー)"""
+        await ctx.send('https://i.gyazo.com/6e1c942142ce952e7b15c8f1aa6e6d73 \n 水が弾けたら')
+        await ctx.message.delete()
+    @bot.command()
+    async def makoto(ctx):
+        """マコト"""
+        await ctx.send('https://i.gyazo.com/016e0804ce330f11c6fcf75a60f89277 \n 黄色の鳴き声が消えたら')
+        await ctx.message.delete()
+    @bot.command()
+    async def muimi(ctx):
+        """ムイミ"""
+        await ctx.send('https://i.gyazo.com/016e0804ce330f11c6fcf75a60f89277 \n 左斜め後ろに飛んだ時')
+        await ctx.message.delete()
+    @bot.command()
+    async def tamaki(ctx):
+        """タマキ"""
+        await ctx.send('https://i.gyazo.com/ede0b883427fbbe2dbd944d03f0d3030')
+        await ctx.message.delete()     
+    @bot.command()
+    async def onon(ctx):
+        """ニノン(オーエド)"""
+        await ctx.send('https://gyazo.com/9ef7490093e2ce4114fc33b49b68a9f9')
+        await ctx.message.delete()
+    @bot.command()
+    async def mizuna(ctx):
+        """スズナ(サマー)"""
+        await ctx.send('https://i.gyazo.com/2b2c3b9dc2856a1d55d167bf920af87d')
+        await ctx.message.delete()
+    @bot.command()
+    async def neneka(ctx):
+        """ネネカ"""
+        await ctx.send('https://i.gyazo.com/588cb732059044e1be0712694eadcffd')
+        await ctx.message.delete()
+    @bot.command()
+    async def puripeko(ctx):
+        """ペコリーヌ(プリンセス)"""
+        await ctx.send('https://gyazo.com/0f0514e31d8b6136f87c33184b649b8d \n 切った後に剣を振り上げたタイミングでバフ付与')
+        await ctx.message.delete()
+    @bot.command()
+    async def mizukyaru(ctx):
+        """キャル(サマー)"""
+        await ctx.send('https://i.gyazo.com/cf8625a0f3fbe7ce5a47f00e2305568a')
+        await ctx.message.delete()
+    @bot.command()
+    async def purikoro(ctx):
+        """コッコロ(プリンセス)"""
+        await ctx.send('https://gyazo.com/04ffe3e6e434d79c816a837e435c1ff1')
+        await ctx.message.delete()    
+    @bot.command()
+    async def harokyo(ctx):
+        """キョウカ(ハロウィン)"""
+        await ctx.send('https://i.gyazo.com/60d59c64eb5c8a9443bd4de074cc03de \n 箒を突き刺した時/箒を振り終わった後')
+        await ctx.message.delete()
+    @bot.command()
+    async def mizusaren(ctx):
+        """サレン(サマー)"""
+        await ctx.send('https://gyazo.com/c22d7996cb2d8e91ae7308b53c66d6d4 \n 右を向いた時')
+        await ctx.message.delete()
+    @bot.command()
+    async def nyukkoro(ctx):
+        """コッコロ(ニューイヤー)"""
+        await ctx.send('https://gyazo.com/c29e006bf82ce2fb0e3b44c06405a9ce \n 左を向いた時')
+        await ctx.message.delete()    
+    @bot.command()
+    async def haromimi(ctx):
+        """ミミ(ハロウィン)"""
+        await ctx.send('https://i.gyazo.com/42f21611ee06890d48f4ffd4d38e6476 \n ぴょん ぴょん ぴょこ←ｺｺ')
+        await ctx.message.delete()
+    @bot.command()
+    async def bazuru(ctx):
+        """シズル(バレンタイン)"""
+        await ctx.send('https://i.gyazo.com/87b47efe83e874247b66519ce2caa78b')
+        await ctx.message.delete()
+    @bot.command()
+    async def kuritika(ctx):
+        """チカ(クリスマス)"""
+        await ctx.send('https://i.gyazo.com/d9b5fcdfca2cac56f82cfd1c9feefd9d \n 足元に魔法陣が出たら/tp配布の数字を見る')
+        await ctx.message.delete()    
+    @bot.command()
+    async def yuni(ctx):
+        """ユニ"""
+        await ctx.send('https://gyazo.com/67a7c23623583779f069e697df22aa23 \n どちらもジャンプ着地時')
+        await ctx.message.delete()
+    @bot.command()
+    async def rei(ctx):
+        """レイ"""
+        await ctx.send('https://i.gyazo.com/4dd4fbfc1687f4529e2f474a84c7a991')
+        await ctx.message.delete()
+    @bot.command()
+    async def suzuna(ctx):
+        """スズナ"""
+        await ctx.send('https://gyazo.com/0f21de3dba9d6d69dee31ef7991b71fc')
+        await ctx.message.delete()
+    @bot.command()
+    async def an(ctx):
+        """アン"""
+        await ctx.send('https://gyazo.com/4771ba5961617b5d66e8fe2aacdc3562')
+        await ctx.message.delete()
+    @bot.command()
+    async def misato(ctx):
+        """ミサト"""
+        await ctx.send('https://gyazo.com/d759d75e36acd6942f1e20d5bf5654ba')
+        await ctx.message.delete() 
+    @bot.command()
+    async def mizumaho(ctx):
+        """マホ(サマー)"""
+        await ctx.send('https://gyazo.com/2848612666b463901e2960c0282685b5')
+        await ctx.message.delete()
+    @bot.command()
+    async def haromiso(ctx):
+        """ミソギ(ハロウィン)"""
+        await ctx.send('https://gyazo.com/667afa5b86200885f706cd8cf65e7651')
+        await ctx.message.delete()
+    @bot.command()
+    async def hiyori(ctx):
+        """ヒヨリ"""
+        await ctx.send('https://gyazo.com/0fceb161f3ddeb1807f048271416525f')
+        await ctx.message.delete()
+    @bot.command()
+    async def mizuhatsune(ctx):
+        """ハツネ(サマー)"""
+        await ctx.send('https://gyazo.com/84891d66caf2de0092a9041020d9145e')
+        await ctx.message.delete()  
 
 @bot.command()
 async def motikosi(ctx, boss: int, p1: int, p2: int):
@@ -249,6 +256,7 @@ async def motikosi(ctx, boss: int, p1: int, p2: int):
     else:
         text = "持ち越しは発生しません"
     await ctx.send(text)
+
 
 
 
